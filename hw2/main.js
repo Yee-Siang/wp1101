423546c99efc5c3b72fde0eaf87de5b53b6609f5
Yee-Siang <ccs1419@gmail.com>
2021-11-21 16:21:57 +0800

    new

--- hw2/main.js
+++ hw2/main.js
@@ -21,11 +21,20 @@ function selectAlbum(x) {
     document.getElementById("album" + String(x)).style.display = 'block';
     if (x === 1) {
         enlarge(1);
+        document.getElementById("B1").style.backgroundColor = '#136b16';
+        document.getElementById("B2").style.backgroundColor = 'rgb(130, 243, 55)';
+        document.getElementById("B3").style.backgroundColor = 'rgb(130, 243, 55)';
     }
     if (x === 2) {
         enlarge(16);
+        document.getElementById("B1").style.backgroundColor = 'rgb(130, 243, 55)';
+        document.getElementById("B2").style.backgroundColor = '#136b16';
+        document.getElementById("B3").style.backgroundColor = 'rgb(130, 243, 55)';
     }
     if (x === 3) {
+        document.getElementById("B1").style.backgroundColor = 'rgb(130, 243, 55)';
+        document.getElementById("B2").style.backgroundColor = 'rgb(130, 243, 55)';
+        document.getElementById("B3").style.backgroundColor = '#136b16';
         window.alert("Sorry! It is an empty album.")
     }
 }
