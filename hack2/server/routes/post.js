624e4a9c3d4fe7c5bf03b1d96f7ab2f3a9b5e44a
Yee-Siang <ccs1419@gmail.com>
2021-12-07 12:03:52 +0800

    new

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,68 @@
+import express from 'express'
+import Post from '../models/post'
+import moment from 'moment'
+
+const router = express.Router()
+
+const saveUser = async (title, content) => {
+    const existing = await Post.findOne({ name: name, subject: subject });
+    //if (existing) throw new Error(`data ${name} exists!!`);
+    if (existing) {
+        try {
+
+            ////////////////////
+            var myquery = { name: name, subject: subject };
+            var newvalues = { $set: { score: score } };
+            User.updateOne(myquery, newvalues, function (err, res) { })
+            ///////////////////
+            return `Updating(${name},${subject},${score})`;
+        } catch (e) { throw new Error("User Criterion Error: " + e); }
+    }
+    else {
+        try {
+            const newUser = new User({ name, subject, score });
+            console.log("Created user", newUser);
+            const re = newUser.save();
+            return `Adding(${name},${subject},${score})`;
+        } catch (e) { throw new Error("User Criterion Error: " + e); }
+    }
+
+};
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+router.get('/api/allPosts', async (req, res) => {
+    try {
+        ////////////////////
+        const allPost = await Post.find({}).sort({ timestamp: -1 });
+        ///////////////////
+
+        console.log(allPost);
+        res.status(200).send({ message: 'success', data: [allPost] });
+    } catch (e) { res.status(403).send({ message: "error", data: null }) }
+
+})
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+router.get('/api/postDetail', async (req, res) => {
+    let postId = req.body.pid;
+    let existing;
+    existing = await Post.findOne({ postId: `${postId}` });
+    if (existing) {
+        res.status(200).send({ message: "success", post: existing })
+    }
+    else {
+        res.status(403).send({ message: "error", post: null })
+    }
+
+})
+
+// TODO 4-(1): create the 3rd API (/api/newPost)
+router.post('/api/newPost', async (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    res.json({ message: `${content}`, card: 'ok' })
+})
+
+
+// TODO 5-(1): create the 4th API (/api/post)
+
+export default router
